39efb687454b86490c9df0f3f0bd43cdd8a924c0
emdevelopa <gyimahsunday@gmail.com>
2024-09-30 22:38:31 +0100

    first_commit

---
+++ template/app.js
@@ -0,0 +1,28 @@
+// in javascript we have
+/* 
+Keywords e.g let, const, function, var, math e.t.c
+variables 
+Data-types such as string, boolean, number e.t.c
+*/
+
+let x = 2;
+// x = 6; variable re-assignment
+
+let addition = 3 + 6
+let multiplication = 2 * 4 
+let division = 4 / 2
+let subtraction=987-87
+
+console.log( "this is the result for addition: " + addition);
+console.log("this is x: " + x)
+
+console.log("this is multiplicaation: " + multiplication);
+console.log("this is division: " + division);
+console.log("this is the output of subtraction:" + subtraction)
+
+
+// strings
+let aboutMe = "I am emmanuel, i code in javascrript."
+let aboutMe2 = 'I am hameedah i code in javascript.'
+// String concatenation
+let string1 = ""
